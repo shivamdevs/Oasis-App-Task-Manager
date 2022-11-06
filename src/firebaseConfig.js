cf4e2923d067ec580f40c76e5580609743252988
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2022-11-06 19:10:56 +0530

    Update code 3.a

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxukV4ANIr5hxQy0t6ALyC40j3uNV1Kt8",
+    authDomain: "shivamdevs-task-manager.firebaseapp.com",
+    projectId: "shivamdevs-task-manager",
+    storageBucket: "shivamdevs-task-manager.appspot.com",
+    messagingSenderId: "610515433025",
+    appId: "1:610515433025:web:ed73778afd081530e378c0",
+    measurementId: "G-1E00NP0QX4"
+};
+
+export default firebaseConfig;
